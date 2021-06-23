ff3c933ea61b4defc847238ab799b0e443335352
yoshihiro <yossy8931@gmail.com>
2021-06-23 21:33:00 +0900

    データ挿入追加

--- mainte/insert.php
+++ mainte/insert.php
@@ -1,16 +1,17 @@
 <?php
 
+function insertContact($request){
 require 'db_connection.php';
 
 $params = [
     'id' => null,
-    'your_name' => 'yama',
-    'email' => 'tes@test.com',
-    'url' => 'https://test.com',
-    'gender' => '1',
-    'age' => '3',
-    'contact' => 'こんにち！',
-    'create_at' => null
+    'your_name' => $request['your_name'],
+    'email' => $request['email'],
+    'url' => $request['url'],
+    'gender' => $request['gender'],
+    'age' => $request['age'],
+    'contact' => $request['contact'],
+    'created_at' => null
 ];
 
 $count = 0;
@@ -20,18 +21,17 @@ $values = '';
 foreach(array_keys($params) as $key){
     if($count++>0){
         $columns .= ',';
-        $values .= '.';
+        $values .= ',';  //.と間違えないように
     }
     $columns .= $key;
     $values .= ':'.$key;
 }
 
-$sql = 'insert into contacts(?,?,?,?,?,?,?,?,)values('. $values .')';
+$sql = 'insert into Contacts ('. $columns .')values('. $values .')';
 
 var_dump($sql);
 
 $stmt = $pdo->prepare($sql);
 $stmt->execute($params);
 
-//$sql = 'insert into contacts(id,your_name,email,url,gender,age,contact,create_at)
-//        values(id,your_name,email,url,gender,age,contact,create_at)';
+}
